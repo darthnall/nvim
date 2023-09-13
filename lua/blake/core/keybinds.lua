74bdf9e645d14611b89171fc935a039fcee5d69d
darthnall <blakenall@proton.me>
2023-09-13 17:08:40 -0500

    Added notifications

--- lua/blake/core/keybinds.lua
+++ lua/blake/core/keybinds.lua
@@ -2,8 +2,11 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-
+-- normal mode
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find File" })
+-- insert mode
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+-- terminal mode
 keymap.set("t", "<ESC>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
---
+++ lua/blake/plugins/sniprun.lua
@@ -0,0 +1,6 @@
+return {
+    "michaelb/sniprun",
+    event = "BufReadPre",
+    dependencies = { "rcarriga/nvim-notify" },
+    opts = { notification_timeout = 5 },
+}
