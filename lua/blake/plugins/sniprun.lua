547e152e56a5964dc3ec0abe668602b80353e012
Blake Nall <blakenall@proton.me>
2023-09-13 18:12:31 -0500

    Added Discord presence

--- lua/blake/plugins/sniprun.lua
+++ lua/blake/plugins/sniprun.lua
@@ -2,5 +2,5 @@ return {
     "michaelb/sniprun",
     event = "BufReadPre",
     dependencies = { "rcarriga/nvim-notify" },
-    opts = { notification_timeout = 5 },
+    config = true,
 }
---
+++ lua/blake/plugins/startup/discord.lua
@@ -0,0 +1,7 @@
+return {
+    "andweeb/presence.nvim",
+    lazy = false,
+    opts = {
+        main_image = "neovim",
+    }
+}
