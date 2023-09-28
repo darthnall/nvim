811a75da26546c6a0b1a006f86746099e4f6be98
darthnall <blakenall@proton.me>
2023-09-27 23:52:21 -0500

    Added new keybinds

--- lua/blake/core/keybinds.lua
+++ lua/blake/core/keybinds.lua
@@ -5,6 +5,10 @@ local keymap = vim.keymap
 -- normal mode
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find File" })
+keymap.set("n", "<leader>h", "<C-w>h", { desc = "Navigate left pane" })
+keymap.set("n", "<leader>j", "<C-w>j", { desc = "Navigate down pane" })
+keymap.set("n", "<leader>k", "<C-w>k", { desc = "Navigate up pane" })
+keymap.set("n", "<leader>l", "<C-w>l", { desc = "Navigate right pane" })
 -- insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 -- terminal mode
