9d98901c47737a508c2391a9007bf0f82f5eddcd
Blake Nall (darthnall) <blakenall@proton.me>
2024-03-04 12:02:09 -0600

    Fixed formatting

--- lua/blake/plugins/copilot.lua
+++ lua/blake/plugins/copilot.lua
@@ -1,17 +1,19 @@
 return {
-    {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        event = "InsertEnter",
-        opts = {
-            panel = {
-                auto_refresh = true
-            }
-        }
-    },
-    {
-        "zbirenbaum/copilot-cmp",
-        config = function() require("copilot_cmp").setup() end,
-        event = "InsertEnter"
-    },
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		opts = {
+			panel = {
+				auto_refresh = true,
+			},
+		},
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+		event = "InsertEnter",
+	},
 }
--- lua/blake/plugins/startup/lualine.lua
+++ lua/blake/plugins/startup/lualine.lua
@@ -2,5 +2,5 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     lazy = false,
-    config = true,
+    opts = { theme = "jellybeans" }
 }
