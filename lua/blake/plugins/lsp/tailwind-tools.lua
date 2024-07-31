4511f1f28e41889daadd8b7dc60b584186b29e76
Blake Nall <blake@proton.me>
2024-07-31 12:00:29 -0500

    Reorganized tailwind classes

--- lua/blake/plugins/lsp/tailwind-tools.lua
+++ lua/blake/plugins/lsp/tailwind-tools.lua
@@ -1,21 +1,25 @@
 return {
 	"luckasRanarison/tailwind-tools.nvim",
-	ft = { "html", "css", "javascript", "typescript" },
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	opts = {
-		document_color = {
-			enabled = true,
-			kind = "inline",
-			inline_symbol = "- ",
-			debounce = 200,
-		},
-		conceal = {
-			enabled = false,
-			min_length = 8,
-			symbol = "*",
-			highlight = {
-				fg = "#388DF8",
+	config = function()
+		local keymap = vim.keymap
+		require("tailwind-tools").setup({
+			document_color = {
+				enabled = true,
+				kind = "inline",
+				inline_symbol = " ",
+				debounce = 200,
 			},
-		},
-	},
+			conceal = {
+				enabled = false,
+				min_length = 8,
+				symbol = "",
+				highlight = {
+					fg = "#388DF8",
+				},
+			},
+		})
+		keymap.set("n", "<leader>T", ":TailwindColorToggle<CR>", { desc = "Toggle tailwind colors." })
+		keymap.set("n", "<leader>tws", ":TailwindSort<CR>", { desc = "Sort all tailwind classes in all buffers." })
+	end,
 }
