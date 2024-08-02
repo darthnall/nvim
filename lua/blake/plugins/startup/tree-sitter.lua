8c6f61075c03f56be531c0d9581b98398015eb27
Blake Nall <blake@proton.me>
2024-08-02 15:04:25 -0500

    Updated tree-sitter start conditions

--- lua/blake/plugins/startup/tree-sitter.lua
+++ lua/blake/plugins/startup/tree-sitter.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-    lazy = false,
+    event = { "BufRead", "BufNewFile", "VeryLazy" },
     build = ":TSUpdate",
     opts = {},
     config = function()
