b09eb0b637bd7ce31ae0bcf4d1340842030324ac
Blake Nall <blake@proton.me>
2024-08-12 10:35:58 -0500

    Replaced nerdtree with oil

---
+++ lua/blake/plugins/startup/oil.lua
@@ -0,0 +1,7 @@
+return {
+    "stevearc/oil.nvim",
+    lazy = false,
+    priority = 999,
+    dependencies = { { "echasnovski/mini.icons" , opts = {} }, { "nvim-tree/nvim-web-devicons" } },
+    opts = {},
+}
