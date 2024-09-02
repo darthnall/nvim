c70cb968d32998d19da7aa33662d35665ca2e1c6
Blake Nall (darthnall) <dev@darthnall.com>
2024-09-01 23:56:57 -0500

    Removed discord presence

--- lua/blake/plugins/lsp/lspconfig.lua
+++ lua/blake/plugins/lsp/lspconfig.lua
@@ -6,6 +6,7 @@ return {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 	},
+	opts = {},
 	config = function()
 		local lspconfig = require("lspconfig")
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
