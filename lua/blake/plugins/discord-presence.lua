bf45ce25ad1593e19f22df1f66500028561d8d33
Blake Nall (darthnall) <dev@darthnall.com>
2024-12-01 12:38:30 -0600

    Removed presence and added telescope keybinds

--- lua/blake/plugins/discord-presence.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"andweeb/presence.nvim",
-	event = "VeryLazy",
-	opts = {
-		auto_update = true,
-		neovim_image_text = "Neovim",
-	},
-}
--- lua/blake/plugins/startup/telescope.lua
+++ lua/blake/plugins/startup/telescope.lua
@@ -7,4 +7,9 @@ return {
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
 	opts = {},
+	keys = {
+		{ "<leader>fg", "<cmd>Telescope live_grep<cr>", { "n" }, desc = "Live grep" },
+		{ "<leader>fb", "<cmd>Telescope buffers<cr>", { "n" }, desc = "Buffers" },
+		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", { "n" }, desc = "Help tags" },
+	},
 }
