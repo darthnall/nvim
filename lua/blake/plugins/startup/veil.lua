0e8ef6e2c99f02755f82257660352a72ea16e09f
darthnall <blake@proton.me>
2025-03-31 16:02:48 -0500

    Added veil

---
+++ lua/blake/plugins/startup/veil.lua
@@ -0,0 +1,10 @@
+return {
+	"willothy/veil.nvim",
+	lazy = false,
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope-file-browser.nvim",
+	},
+	opts = {},
+}
